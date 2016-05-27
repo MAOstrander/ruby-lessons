7102f2e68fcb4125fe1e06ce927029dd5d6a7b41
Mathew Ostrander <mathew.ostrander@gmail.com>
2016-05-26 19:29:37 -0500

    quite similar to python so far, .inject is cool

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ruby-lessons
+
+Learning the ropes of Ruby, started with Exercism.io at the Ruby for Beginners Meetup
---
+++ hamming.rb
@@ -0,0 +1,16 @@
+class Hamming
+  VERSION = 2.freeze
+  def self.compute(strand_one, strand_two)
+    return 0 if strand_one == strand_two
+    raise ArgumentError if strand_one.length != strand_two.length
+
+    strand_one = strand_one.chars
+    strand_two = strand_two.chars
+
+    comparison = strand_one.zip(strand_two)
+    comparison.inject(0) do |acc, pair|
+      acc += 1 unless pair.first == pair.last
+      acc
+    end
+  end
+end
